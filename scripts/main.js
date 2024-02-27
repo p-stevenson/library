1922ce7f50ba28ebc54cfde22705ac9cf6f1b7dd
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-02-27 11:53:13 +0000

    Add default values to Book constructor

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,9 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read = false) {
+function Book(title = 'The Hobbit', author = 'J.R.R Tolkien', pages = 200, read = false) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
 	this.uuid = crypto.randomUUID();
 }
-
-Book.prototype.hasRead = function () {
-	return this.read ? 'has read' : 'has not read';
-}
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 271)
