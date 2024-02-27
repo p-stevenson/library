166b69a01e87ad98bc147bafcec1a574c311b005
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-02-27 12:29:48 +0000

    Add hasRead method to Book.prototype

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,7 @@ function Book(title = 'The Hobbit', author = 'J.R.R Tolkien', pages = 200, read 
 	this.read = read;
 	this.uuid = crypto.randomUUID();
 }
+
+Book.prototype.hasRead = () => {
+	return this.read ? 'read' : 'not read';
+};
