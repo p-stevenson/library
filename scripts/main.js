9273a6ed47ca43cfd43e6f96c4985839ead700b8
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-01 15:50:13 +0000

    Add create method to Book prototype
    working but not complete
    will seperate out functionality further create/populate methods
    
    creates and appends container and book information for display.

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,32 @@ function Book() {
 
 Book.prototype.add = function () {
 	myLibrary.push(this);
-	document.getElementById('form-container').style.display = "none"
+	document.getElementById('form-container').style.display = "none";
+};
+
+Book.prototype.create = function () {
+	const container = document.getElementById('books-container');
+	const card = document.createElement('div');
+	card.setAttribute('id', this.uuid);
+	card.classList.add('card');
+	container.appendChild(card);
+	const title = document.createElement('h2');
+	title.textContent = `${this.title}`;
+	const author = document.createElement('h4');
+	author.textContent = `${this.author}`;
+	const pages = document.createElement('h4');
+	pages.textContent = `${this.pages}`;
+	//TODO explore splitting create into create and populate methods
+	//TODO try to implement create more programatically
+	//TODO update card info styling
+	card.appendChild(title);
+	card.appendChild(author);
+	card.appendChild(pages)
 };
 
 //----OPEN FORM----
 (function () {
 	const openFormButton = document.getElementById('openFormButton');
-
 	openFormButton.addEventListener('click', () => {
 		document.getElementById('form-container').style.display = 'block';
 	});
@@ -42,7 +61,9 @@ Book.prototype.add = function () {
 	});
 
 	submitFormButton.addEventListener('click', () => {
-		new Book().add();
+		const tempBook = new Book();
+		tempBook.add();
+		tempBook.create();
 		clearFormFields();
 	});
 })();
@@ -51,3 +72,7 @@ Book.prototype.add = function () {
 function clearFormFields() {
 	document.getElementById('newBookForm').reset();
 }
+
+//----DISPLAY BOOKS ----
+
+
