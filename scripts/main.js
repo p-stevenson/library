bc0b0f151b575bdfb366d1bd3035c99d92842f86
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-04 15:52:00 +0000

    Add setReadStatus method to Book constructor

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 const LIBRARY = [];
 
 function Book() {
-	this.title = document.getElementById('bookTitle').value;
-	this.author = `written by ${document.getElementById('author').value}`;
-	this.pages = `Pages: ${document.getElementById('numOfPages').value}`;
+	this.title = document.getElementById('inputTitle').value;
+	this.author = `written by ${document.getElementById('inputAuthor').value}`;
+	this.pages = `Pages: ${document.getElementById('inputPages').value}`;
 	this.read = document.getElementById('hasRead').checked;
 	this.uuid = crypto.randomUUID();
 }
@@ -20,7 +20,7 @@ Book.prototype.createCard = function () {
 Book.prototype.populateCardInfo = function () {
 	const GET_CARD = document.getElementById(`${this.uuid}`);
 	const KEY = [this.title, this.author, this.pages];
-	const TEMP_ID = ['title', 'author', 'numOfpages'];
+	const TEMP_ID = ['title', 'author', 'numOfPages'];
 
 	for (let i in KEY) {
 		const TEMP_ELEMENT = document.createElement('p');
@@ -29,6 +29,28 @@ Book.prototype.populateCardInfo = function () {
 		GET_CARD.appendChild(TEMP_ELEMENT);
 	}
 };
+
+Book.prototype.setReadStatus = function () {
+	const GET_CARD = document.getElementById(`${this.uuid}`);
+	const TEMP_LABEL = document.createElement('label');
+	const TEMP_ELEMENT = document.createElement('input');
+
+	TEMP_LABEL.setAttribute('for', 'readStatus');
+	TEMP_LABEL.setAttribute('name', 'readStatus');
+	TEMP_LABEL.textContent = 'Read Status:';
+
+	TEMP_ELEMENT.setAttribute('type', 'checkbox');
+	TEMP_ELEMENT.setAttribute('id', 'readStatus');
+
+	(this.read === true) ? TEMP_ELEMENT.setAttribute('checked', 'checked') :
+		TEMP_ELEMENT.removeAttribute('checked');
+
+	TEMP_ELEMENT.addEventListener('click', () => {
+		(this.read === true) ? this.read = false : this.read = true
+	})
+	GET_CARD.appendChild(TEMP_LABEL);
+	GET_CARD.appendChild(TEMP_ELEMENT);
+}
 
 Book.prototype.addBook = function () {
 	LIBRARY.push(this);
@@ -67,6 +89,7 @@ Book.prototype.addBook = function () {
 		TEMP_BOOK.addBook();
 		TEMP_BOOK.createCard();
 		TEMP_BOOK.populateCardInfo();
+		TEMP_BOOK.setReadStatus();
 		clearFormFields();
 	});
 })();
